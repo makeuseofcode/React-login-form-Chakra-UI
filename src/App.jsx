2d1bfab95bda3bb8f2532d10f3d6df0810c21bb5
ItsWachira <brianwachira79@gmail.com>
2023-08-22 12:33:26 +0300

    login form styled using chakra UI's components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './components/login'
+import ThemeToggler from './components/ThemeToggler'
+
+export default function App() {
+  return (
+    <div>
+     <ThemeToggler />
+     <Login />
+    </div>
+  )
+}
+
